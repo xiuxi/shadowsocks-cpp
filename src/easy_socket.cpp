bb38e907d2b1a84a573ec6054b973698c16d426b
wine <winelaner@outlook.com>
2018-07-10 11:09:39 +0800

    Update easy_socket.cpp

--- src/easy_socket.cpp
+++ src/easy_socket.cpp
@@ -35,13 +35,13 @@ Socket::Socket(const int domain, const int type, const int protocol): _domain(do
                   
 Socket::Socket(const Socket &s): _domain(s._domain), _ref_count(s._ref_count), _socket_fd(s._socket_fd), _addrs(s._addrs)
 {
-	if (_ref_count) 
-		++*_ref_count;
+	if (s._ref_count) 
+		++*s._ref_count;
 }
 
 Socket& Socket::operator=(const Socket &s)
 {
-    if (_ref_count)
+    if (s._ref_count)
         ++*s._ref_count; 
     
     _destroy();
